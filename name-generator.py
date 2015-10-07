7f365541e9531da28b1623abf94c150a2e273bb1
Matthew Coons <matthewcoons@Matthews-MacBook-Pro.local>
2015-10-06 21:51:51 -0400

    adding fixes

--- name-generator.py
+++ name-generator.py
@@ -2,9 +2,9 @@ import random
 
 word_file = "words.txt"
 WORDS = open(word_file).read().splitlines()
-word1 = 'First Choice: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
-word2 = 'Second Choice: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
-word3 = 'Third Choice: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
+word1 = 'Choice 1: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
+word2 = 'Choice 2: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
+word3 = 'Choice 3: ' + random.choice(WORDS) + ' ' + random.choice(WORDS)
 print word1
 print word2
 print word3
